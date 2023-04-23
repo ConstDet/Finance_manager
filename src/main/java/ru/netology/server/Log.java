09ee49838adeeb89c6748baf1d3b1c6098f2ad6e
ConstDet <constx7@gmail.com>
2023-04-23 14:41:39 +0300

    Отредактировал Log.java

--- src/main/java/ru/netology/server/Log.java
+++ src/main/java/ru/netology/server/Log.java
@@ -23,28 +23,6 @@ public class Log {
         return listRequestLog;
     }
 
-/*    public  void loadLog() throws FileNotFoundException {
-        if (fileJson.exists()) {
-            InputStreamReader iSR = new FileReader(fileJson);
-            StringBuilder strBuild = new StringBuilder();
-            try (BufferedReader BuffR = new BufferedReader(iSR)) {
-                int intStr;
-                while ((intStr = BuffR.read()) != -1) {
-                    strBuild.append(Character.toChars(intStr));
-                }
-                String strB = strBuild.toString();
-                Gson gson = new GsonBuilder().create();
-                Request[] req = gson.fromJson(strB, Request[].class);
-                listRequestLog.clear();
-                for (Request r : req) {
-                    listRequestLog.add(r);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }*/
-
     public  void loadLog() throws FileNotFoundException {
         if (fileBin.exists()) {
             InputStreamReader iSR = new FileReader(fileBin);
