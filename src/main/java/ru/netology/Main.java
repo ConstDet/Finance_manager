eb626068dc5939ffa08e8f111ecf65aa944e9fe4
ConstDet <constx7@gmail.com>
2023-04-23 14:22:08 +0300

    Решена дополнительная задача 1

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,14 +1,11 @@
 package ru.netology;
 
 import ru.netology.server.CalculateMax;
-import ru.netology.server.LoadSaveTSV;
 import ru.netology.server.Log;
-import ru.netology.server.Product;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
 
 public class Main {
     static final int PORT = 8989;
@@ -21,6 +18,7 @@ public class Main {
                      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                     out.println("Создано подключение на сервере через порт " + clientSocket.getLocalPort());
                     log.addLog(in.readLine());//получили сообщение и записали его в лог
+                    log.saveLog();//и сохраним его в файле data.bin
                     CalculateMax calculateMax = new CalculateMax(log);//считаем макс сумму по категориям
                     out.println(calculateMax.calcMax());//ответ
                 }
--- src/main/java/ru/netology/server/Log.java
+++ src/main/java/ru/netology/server/Log.java
@@ -2,14 +2,18 @@ package ru.netology.server;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+//org.fife.io.UnicodeReader
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Log {
     private List<Request> listRequestLog = new ArrayList<>();
     private final File fileJson = new File("jsonLog.json");
+    private final File fileBin = new File("data.bin");
 
     public Log() throws FileNotFoundException {
         loadLog();
@@ -19,7 +23,7 @@ public class Log {
         return listRequestLog;
     }
 
-    public  void loadLog() throws FileNotFoundException {
+/*    public  void loadLog() throws FileNotFoundException {
         if (fileJson.exists()) {
             InputStreamReader iSR = new FileReader(fileJson);
             StringBuilder strBuild = new StringBuilder();
@@ -38,6 +42,41 @@ public class Log {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }*/
+
+    public  void loadLog() throws FileNotFoundException {
+        if (fileBin.exists()) {
+            InputStreamReader iSR = new FileReader(fileBin);
+            StringBuilder strBuild = new StringBuilder();
+            try(BufferedReader BuffR = new BufferedReader(iSR)){
+                int intStr;
+                while ((intStr = BuffR.read()) != -1) {
+                    strBuild.append(Character.toChars(intStr));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String strBis = strBuild.toString();
+            Gson gson = new GsonBuilder().create();
+            Request[] req = gson.fromJson(strBis, Request[].class);
+            listRequestLog.clear();
+            for (Request r : req) {
+                listRequestLog.add(r);
+            }
+        }
+    }
+
+    public void saveLog() throws FileNotFoundException {
+        if (listRequestLog == null) return;
+        Gson gson = new GsonBuilder().create();
+        String str = gson.toJson(listRequestLog);
+        byte[] buffer = str.getBytes();
+        try (FileOutputStream out = new FileOutputStream(fileBin);
+        BufferedOutputStream bos = new BufferedOutputStream(out)) {
+            bos.write(buffer, 0, buffer.length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
